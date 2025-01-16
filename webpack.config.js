d175705b7064540dbf354064f031f14a80f640a5
luciaPG <lucpergut@alum.us.es>
2025-01-16 13:38:13 +0100

    build: updated the depedencies to nowadays versions
    Closes #6

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,29 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: {
-        bundle: ['./app/app.js'], // Archivo de entrada principal
-    },
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './app/PPINOT-modeler/index.js',
     output: {
-        path: path.resolve(__dirname, 'public'), // Carpeta de salida
-        filename: 'app.js', // Archivo generado
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
     module: {
         rules: [{
-            test: /\.bpmn$/, // Procesa archivos .bpmn
-            use: 'raw-loader', // Los carga como cadenas de texto
-        }, ],
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [{
-                    from: 'assets/**',
-                    to: 'vendor/bpmn-js/[name][ext]',
-                    context: path.resolve(__dirname, 'node_modules/bpmn-js/dist/'),
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
                 },
-                {
-                    from: '**/*.{html,css}',
-                    to: '[name][ext]',
-                    context: path.resolve(__dirname, 'app/'),
-                },
-            ],
-        }),
-    ],
-    mode: 'development', // Cambiar a 'production' para construir en producción
-    devtool: 'source-map', // Genera mapas de origen para depuración
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'), // Sirve archivos desde la carpeta 'public'
-        },
-        port: 9000, // Puerto del servidor de desarrollo
-        hot: true, // Habilita Hot Module Replacement
-        open: true, // Abre el navegador automáticamente
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
+    plugins: [],
 };
