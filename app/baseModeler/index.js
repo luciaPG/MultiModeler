2eadaabab46a70b623cd91a5bd17ee4b02ded89a
Lucia <lucpergut@alum.us.es>
2025-06-21 13:33:08 +0200

    fix: fix context pad
    #16

--- app/baseModeler/index.js
+++ app/baseModeler/index.js
@@ -3,16 +3,12 @@ import { assign } from 'min-dash';
 import { isPPINOTConnection } from '../PPINOT-modeler/PPINOT/Types';
 import { isLabelExternal, getLabel } from '../PPINOT-modeler/PPINOT/utils/LabelUtil';
 
-// Base module with common functionality
 import BaseModule from './BaseModule';
-
-// Available notation modules
 import PPINOTModule from '../PPINOT-modeler/PPINOT';
 import RALPHModule from '../RALPH-modeler/RALph';
 
 export default class BaseModeler extends Modeler {
   constructor(options = {}) {
-    // Handle the unified architecture: extract notations from options
     const {
       notations = [],
       enablePPINOT = false,
@@ -21,97 +17,53 @@ export default class BaseModeler extends Modeler {
       ...restOptions
     } = options;
 
-    // Build notations array if not explicitly provided
     const enabledNotations = notations.length > 0 ? notations : [];
     if (notations.length === 0) {
       if (enablePPINOT) enabledNotations.push('ppinot');
       if (enableRALPH) enabledNotations.push('ralph');
-    }    // Configure which notation modules to include
+    }
+
     const notationModules = BaseModeler._getNotationModules(enabledNotations);
-    
-    // Only include BaseModule if we have notations or need custom functionality
     const modulesToInclude = [];
-    
-    // Add BaseModule only if we have enabled notations
     if (enabledNotations.length > 0) {
       modulesToInclude.push(BaseModule);
     }
-      // Add notation-specific modules
     modulesToInclude.push(...notationModules);
-    
-    // Merge modules with ALL options (including container)
+
     const enhancedOptions = assign({}, restOptions, {
       additionalModules: [
         ...modulesToInclude,
         ...(restOptions.additionalModules || [])
       ]
-    });    super(enhancedOptions);
-    
-    this._customElements = [];
-    this._idMap = [];
-    this.modelOpen = false;
-    this._enabledNotations = new Set(enabledNotations);
-    this._notationMode = defaultNotation;
+    });
 
-    console.log('BaseModeler created with notations:', enabledNotations, 'mode:', defaultNotation);
+    super(enhancedOptions);
+
   }
 
-  /**
-   * Get notation modules based on requested notations
-   * @param {Array<string>} notations - Array of notation names ('ppinot', 'ralph')
-   * @returns {Array} Array of notation modules
-   */
   static _getNotationModules(notations) {
     const modules = [];
-    
-    if (notations.includes('ppinot')) {
-      modules.push(PPINOTModule);
-    }
-    
-    if (notations.includes('ralph')) {
-      modules.push(RALPHModule);
-    }
-    
+    if (notations.includes('ppinot')) modules.push(PPINOTModule);
+    if (notations.includes('ralph')) modules.push(RALPHModule);
     return modules;
   }
 
-  /**
-   * Set the notation mode
-   * @param {string} mode - 'ppinot', 'ralph', 'bpmn', or 'mixed'
-   */
   setNotationMode(mode) {
     if (!['ppinot', 'ralph', 'bpmn', 'mixed'].includes(mode)) {
       throw new Error('Invalid notation mode. Must be one of: ppinot, ralph, bpmn, mixed');
     }
-    
     this._notationMode = mode;
-    
-    // Fire event for other components to react
-    if (this._eventBus) {
-      this._eventBus.fire('notation.mode.changed', { mode });
-    }
+    this._eventBus?.fire('notation.mode.changed', { mode });
   }
 
-  /**
-   * Get current notation mode
-   */
   getNotationMode() {
     return this._notationMode;
   }
 
-  /**
-   * Check if a notation is enabled
-   * @param {string} notation - The notation to check
-   * @returns {boolean}
-   */
   hasNotation(notation) {
     return this._enabledNotations.has(notation);
   }
 
-  /**
-   * Get list of enabled notations
-   * @returns {Array<string>}
-   */
   getEnabledNotations() {
     return Array.from(this._enabledNotations);
   }
@@ -137,138 +89,94 @@ export default class BaseModeler extends Modeler {
     this._customElements.push(...elements);
   }
 
-  // PPINOT-specific methods (only available if PPINOT notation is enabled)
   _addPPINOTShape(PPINOTElement) {
     if (!this.hasNotation('ppinot')) {
       throw new Error('PPINOT notation is not enabled');
     }
-
     this._customElements.push(PPINOTElement);
-    const canvas = this.get('canvas');
-    const elementFactory = this.get('elementFactory');
-    const PPINOTAttrs = assign({ businessObject: PPINOTElement }, PPINOTElement);
-    const PPINOTShape = elementFactory.create('shape', PPINOTAttrs);
-
-    if (isLabelExternal(PPINOTElement) && getLabel(PPINOTShape)) {
-      this.addLabel(PPINOTElement, PPINOTShape);
+    const shape = this.get('elementFactory').create('shape', assign({ businessObject: PPINOTElement }, PPINOTElement));
+    if (isLabelExternal(PPINOTElement) && getLabel(shape)) {
+      this.addLabel(PPINOTElement, shape);
     }
-
-    return canvas.addShape(PPINOTShape);
+    return this.get('canvas').addShape(shape);
   }
 
   _addPPINOTConnection(PPINOTElement) {
     if (!this.hasNotation('ppinot')) {
       throw new Error('PPINOT notation is not enabled');
     }
-
     this._customElements.push(PPINOTElement);
-    const canvas = this.get('canvas');
     const elementFactory = this.get('elementFactory');
     const elementRegistry = this.get('elementRegistry');
-
-    const PPINOTAttrs = assign({
-      businessObject: PPINOTElement
-    }, PPINOTElement);
-
+    const attrs = assign({ businessObject: PPINOTElement }, PPINOTElement);
     if (PPINOTElement.source && PPINOTElement.target) {
-      PPINOTAttrs.source = elementRegistry.get(PPINOTElement.source);
-      PPINOTAttrs.target = elementRegistry.get(PPINOTElement.target);
+      attrs.source = elementRegistry.get(PPINOTElement.source);
+      attrs.target = elementRegistry.get(PPINOTElement.target);
     }
-
-    const PPINOTConnection = elementFactory.createConnection(PPINOTAttrs);
-
-    if (isLabelExternal(PPINOTElement) && getLabel(PPINOTConnection)) {
-      this.addLabel(PPINOTElement, PPINOTConnection);
+    const connection = elementFactory.createConnection(attrs);
+    if (isLabelExternal(PPINOTElement) && getLabel(connection)) {
+      this.addLabel(PPINOTElement, connection);
     }
-
-    return canvas.addConnection(PPINOTConnection);
+    return this.get('canvas').addConnection(connection);
   }
 
   addPPINOTElement(PPINOTElement) {
-    if (isPPINOTConnection(PPINOTElement)) {
-      return this._addPPINOTConnection(PPINOTElement);
-    } else {
-      return this._addPPINOTShape(PPINOTElement);
-    }
+    return isPPINOTConnection(PPINOTElement)
+      ? this._addPPINOTConnection(PPINOTElement)
+      : this._addPPINOTShape(PPINOTElement);
   }
 
   importPPINOTDiagram(PPINOTElements) {
     if (!this.hasNotation('ppinot')) {
       throw new Error('PPINOT notation is not enabled');
     }
-    
-    PPINOTElements.forEach(element => {
-      this.addPPINOTElement(element);
-    });
+    PPINOTElements.forEach(el => this.addPPINOTElement(el));
   }
 
-  // RALPH-specific methods (only available if RALPH notation is enabled)
   _addRALPHShape(RALPHElement) {
     if (!this.hasNotation('ralph')) {
       throw new Error('RALPH notation is not enabled');
     }
-
     this._customElements.push(RALPHElement);
-    const canvas = this.get('canvas');
-    const elementFactory = this.get('elementFactory');
-    const RALPHAttrs = assign({ businessObject: RALPHElement }, RALPHElement);
-    const RALPHShape = elementFactory.create('shape', RALPHAttrs);
-
-    if (isLabelExternal(RALPHElement) && getLabel(RALPHShape)) {
-      this.addLabel(RALPHElement, RALPHShape);
+    const shape = this.get('elementFactory').create('shape', assign({ businessObject: RALPHElement }, RALPHElement));
+    if (isLabelExternal(RALPHElement) && getLabel(shape)) {
+      this.addLabel(RALPHElement, shape);
     }
-
-    return canvas.addShape(RALPHShape);
+    return this.get('canvas').addShape(shape);
   }
 
   _addRALPHConnection(RALPHElement) {
     if (!this.hasNotation('ralph')) {
       throw new Error('RALPH notation is not enabled');
     }
-
     this._customElements.push(RALPHElement);
-    const canvas = this.get('canvas');
     const elementFactory = this.get('elementFactory');
     const elementRegistry = this.get('elementRegistry');
-
-    const RALPHAttrs = assign({
-      businessObject: RALPHElement
-    }, RALPHElement);
-
+    const attrs = assign({ businessObject: RALPHElement }, RALPHElement);
     if (RALPHElement.source && RALPHElement.target) {
-      RALPHAttrs.source = elementRegistry.get(RALPHElement.source);
-      RALPHAttrs.target = elementRegistry.get(RALPHElement.target);
+      attrs.source = elementRegistry.get(RALPHElement.source);
+      attrs.target = elementRegistry.get(RALPHElement.target);
     }
-
-    const RALPHConnection = elementFactory.createConnection(RALPHAttrs);
-
-    if (isLabelExternal(RALPHElement) && getLabel(RALPHConnection)) {
-      this.addLabel(RALPHElement, RALPHConnection);
+    const conn = elementFactory.createConnection(attrs);
+    if (isLabelExternal(RALPHElement) && getLabel(conn)) {
+      this.addLabel(RALPHElement, conn);
     }
-
-    return canvas.addConnection(RALPHConnection);
+    return this.get('canvas').addConnection(conn);
   }
 
   addRALPHElement(RALPHElement) {
-    // Simple type check for RALPH connections
-    if (RALPHElement.source && RALPHElement.target) {
-      return this._addRALPHConnection(RALPHElement);
-    } else {
-      return this._addRALPHShape(RALPHElement);
-    }
+    return RALPHElement.source && RALPHElement.target
+      ? this._addRALPHConnection(RALPHElement)
+      : this._addRALPHShape(RALPHElement);
   }
 
   importRALPHDiagram(RALPHElements) {
     if (!this.hasNotation('ralph')) {
       throw new Error('RALPH notation is not enabled');
     }
-    
-    RALPHElements.forEach(element => {
-      this.addRALPHElement(element);
-    });
+    RALPHElements.forEach(el => this.addRALPHElement(el));
   }
 
-  // Mixed notation methods
   importMixedDiagram(mixedData) {
     if (mixedData.ppinot && this.hasNotation('ppinot')) {
       this.importPPINOTDiagram(mixedData.ppinot);
@@ -278,23 +186,18 @@ export default class BaseModeler extends Modeler {
     }
   }
 
-  // Color setting method (used by both PPINOT and RALPH)
   setColors(idAndColorList) {
     const modeling = this.get('modeling');
     const elementRegistry = this.get('elementRegistry');
-
     idAndColorList.forEach(obj => {
-      let element;
-      obj.ids.forEach(id => {
-        if (this._idMap[id]) {
-          element = this._idMap[id].map(i => elementRegistry.get(i));
-        } else if (elementRegistry.get(id) && elementRegistry.get(id).type === 'bpmn:Task') {
-          element = elementRegistry.get(id);
-        }
-      });
-
-      if (element != null) {
-        modeling.setColor(element, {
+      let elems;
+      if (this._idMap[obj.id]) {
+        elems = this._idMap[obj.id].map(i => elementRegistry.get(i));
+      } else if (elementRegistry.get(obj.id)?.type === 'bpmn:Task') {
+        elems = [elementRegistry.get(obj.id)];
+      }
+      if (elems) {
+        modeling.setColor(elems, {
           stroke: obj.fillColor !== '#ffffff' ? obj.fillColor : 'green',
           fill: '#fff'
         });
@@ -302,80 +205,40 @@ export default class BaseModeler extends Modeler {
     });
   }
 
-  // Unified modeler helper methods
-  
-  /**
-   * Enable a notation dynamically
-   * @param {string} notation - The notation to enable ('ppinot' or 'ralph')
-   */
   enableNotation(notation) {
     if (!['ppinot', 'ralph'].includes(notation)) {
       throw new Error('Invalid notation. Must be "ppinot" or "ralph"');
     }
-    
     if (!this._enabledNotations.has(notation)) {
       this._enabledNotations.add(notation);
       console.log(`Notation "${notation}" enabled.`);
-      
-      // Fire event for palette and other components to update
-      if (this._eventBus) {
-        this._eventBus.fire('notation.enabled', { notation });
-      }
-      
-      // Refresh palette if available
+      this._eventBus?.fire('notation.enabled', { notation });
       this._refreshPalette();
     }
   }
 
-  /**
-   * Disable a notation
-   * @param {string} notation - The notation to disable
-   */
   disableNotation(notation) {
     if (this._enabledNotations.has(notation)) {
       this._enabledNotations.delete(notation);
       console.log(`Notation "${notation}" disabled.`);
-      
-      // Fire event for palette and other components to update
-      if (this._eventBus) {
-        this._eventBus.fire('notation.disabled', { notation });
-      }
-      
-      // Refresh palette if available
+      this._eventBus?.fire('notation.disabled', { notation });
       this._refreshPalette();
     }
   }
-  /**
-   * Refresh the palette after notation changes
-   * @private
-   */
+
   _refreshPalette() {
     try {
-      const palette = this.get('palette');
-      if (palette && palette.refresh) {
-        palette.refresh();
-      }
-      
-      const eventBus = this.get('eventBus');
-      if (eventBus) {
-        eventBus.fire('palette.refresh');
-      }
-    } catch (error) {
-      console.warn('Could not refresh palette:', error.message);
+      this.get('palette')?.refresh();
+      this.get('eventBus')?.fire('palette.refresh');
+    } catch (err) {
+      console.warn('Could not refresh palette:', err.message);
     }
   }
 
-  /**
-   * Check if this is the unified modeler (always true for BaseModeler)
-   * @returns {boolean}
-   */
   isUnifiedModeler() {
     return true;
   }
-  /**
-   * Get configuration summary
-   * @returns {Object}
-   */
+
   getConfiguration() {
     return {
       type: 'unified',
